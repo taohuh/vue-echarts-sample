96bb2f4fb27d055477b482274c536f207dafc485
taohuh <pimpakarn.wan@gmail.com>
2019-09-02 11:35:31 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,117 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ECharts Sample !</h1>
+    <h3>Bar chart</h3>
+    <v-chart :options="chartOptionsBar"/>
+    <h3>Basic Radar Chart</h3>
+    <v-chart :options="chartOptionsBasicRadar"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ECharts from 'vue-echarts'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'v-chart': ECharts
+  },
+  data() {
+    return {
+      chartOptionsBar: {
+        xAxis: {
+          data: ['Q1', 'Q2', 'Q3', 'Q4']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            type: 'bar',
+            data: [63, 75, 24, 92]
+          }
+        ]
+      },
+      chartOptionsBasicRadar: {
+        title: {
+          text: 'Basic Radar Chart'
+        },
+        legend: {
+          bottom: 0,
+          data: [
+            'Tao',
+            'Big',
+            'Todz',
+            'Benz',
+            'Tai',
+          ]
+        },
+        radar: {
+          name: {
+            textStyle: {
+              color: '#fff',
+              backgroundColor: '#999',
+              borderRadius: 3,
+              padding: [3, 5]
+            }
+          },
+          indicator: [
+            {
+              name: 'HTML & CSS',
+              max: 100
+            },
+            {
+              name: 'Javascript',
+              max: 100
+            },
+            {
+              name: 'VueJS',
+              max: 100
+            },
+            {
+              name: 'React',
+              max: 100
+            },
+            {
+              name: 'SEO',
+              max: 100
+            },
+            {
+              name: 'Animation',
+              max: 100
+            }
+          ]
+        },
+        series: [
+          {
+            name: 'Test',
+            type: 'radar',
+            data: [
+              {
+                value: [20, 50, 53, 50, 65, 50],
+                name: 'Tao'
+              },
+              {
+                value: [40, 80, 50, 55, 34, 50],
+                name: 'Big'
+              },
+              {
+                value: [100, 50, 89, 50, 30, 20],
+                name: 'Todz'
+              },
+              {
+                value: [20, 50, 99, 76, 30, 50],
+                name: 'Benz'
+              },
+              {
+                value: [50, 40, 100, 99, 24, 43],
+                name: 'Tai'
+              }
+            ]
+          }
+        ]
+      }
+    }
   }
 }
 </script>
@@ -24,5 +124,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ECharts from 'vue-echarts/components/ECharts'
+
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/radar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/legend'
+
+Vue.component('v-chart', ECharts)
 
 Vue.config.productionTip = false
 
